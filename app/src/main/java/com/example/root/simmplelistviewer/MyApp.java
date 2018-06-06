b7825ac6047a020231db75eab007bf0297679b2c
heronzo <odhiamboherons12@gmail.com>
2018-06-06 17:17:15 +0300

    Context name changed in manifest

--- app/src/main/java/com/example/root/simmplelistviewer/MyApp.java
+++ app/src/main/java/com/example/root/simmplelistviewer/MyApp.java
@@ -10,15 +10,14 @@ public class MyApp extends Application {
     public static String TAG =MyApp.class.getSimpleName();
     private static MyApp session;
     private static BoxStore boxStore;
-    public static MyApp getSession(){
-        return session;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
-        session = this;
+        session = MyApp.this;
         boxStore = MyObjectBox.builder().androidContext(MyApp.this).build();
+    }
+    public static MyApp getSession(){
+        return session;
     }
     public BoxStore getMyBoxStore() {
         return boxStore;
